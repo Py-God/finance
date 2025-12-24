7c45eaef592c6af96ddf44105d50f2fc6c67b86a
Boluwatife <boluwatifelekeoduoye@gmail.com>
2025-12-24 06:47:31 +0100

    added schema.sql

---
+++ schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    username TEXT NOT NULL, 
+    hash TEXT NOT NULL, 
+    cash NUMERIC NOT NULL DEFAULT 10000.00
+);
+
+CREATE TABLE transactions (
+    user_id INTEGER NOT NULL, 
+    symbol TEXT NOT NULL, 
+    shares INTEGER NOT NULL, 
+    price NUMERIC NOT NULL, 
+    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+CREATE TABLE history (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL, 
+    symbol TEXT NOT NULL, 
+    shares INTEGER NOT NULL, 
+    price NUMERIC NOT NULL, 
+    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+CREATE UNIQUE INDEX username ON users (username);
